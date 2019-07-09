fffee1f7203614c7d7e137e76b1c6940839515f6
pclightyear <ee91608@gmail.com>
2019-07-09 17:02:42 +0800

    (HomePagerAdapter.java) Just display dummy message so far.

---
+++ app/src/main/java/com/speedrun_mobile_unofficial/HomePagerAdapter.java
@@ -0,0 +1,41 @@
+package com.speedrun_mobile_unofficial;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Custom adapter for homepage ViewPager.
+ */
+public class HomePagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public HomePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
