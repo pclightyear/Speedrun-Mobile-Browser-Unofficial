8616eb0dc6032f9d65f77c02b720e703121dbce6
pclightyear <ee91608@gmail.com>
2019-07-10 11:17:27 +0800

    (GamePlaceholderFragment.java) Finish GridView layout display. TODO: Implement item click listener to jump to leaderboard activity.

--- app/src/main/java/com/speedrun_mobile_unofficial/GamePlaceholderFragment.java
+++ app/src/main/java/com/speedrun_mobile_unofficial/GamePlaceholderFragment.java
@@ -1,11 +1,15 @@
 package com.speedrun_mobile_unofficial;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -14,6 +18,8 @@ import android.widget.TextView;
  * homepage.
  */
 public class GamePlaceholderFragment extends Fragment {
+    private GridView gameGridView;
+    private GamePlaceholderFragmentAdapter gamePlaceholderFragmentAdapter;
 
     public GamePlaceholderFragment() {
     }
@@ -21,9 +27,19 @@ public class GamePlaceholderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        TextView textView = rootView.findViewById(R.id.section_label);
-        textView.setText(R.string.SUBSCRIBED_GAMES_NOT_FINISHED);
+        View rootView = inflater.inflate(R.layout.fragment_game, container, false);
+        gameGridView = rootView.findViewById(R.id.game_grid);
+        gamePlaceholderFragmentAdapter = new GamePlaceholderFragmentAdapter(this.getContext());
+        gameGridView.setAdapter(gamePlaceholderFragmentAdapter);
+        gameGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(GamePlaceholderFragment.this.getContext(), Integer.toString(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+//        textView.setText(R.string.SUBSCRIBED_GAMES_NOT_FINISHED);
         return rootView;
     }
+
+
 }
